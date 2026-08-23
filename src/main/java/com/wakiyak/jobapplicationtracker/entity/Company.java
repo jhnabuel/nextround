1be29dbbc6494e21330b1b34bf84a8a8060d1ade
John Abuel <ninobano43@gmail.com>
2026-08-23 21:01:29 +0800

    feat: add Company entity with JPA annotations and audit timestamps

---
+++ src/main/java/com/wakiyak/jobapplicationtracker/entity/Company.java
@@ -0,0 +1,42 @@
+package com.wakiyak.jobapplicationtracker.entity;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+ class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String companyName;
+
+    private String websiteUrl;
+
+    private String location;
+
+    @Column(nullable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    protected Company(){}
+
+    public Company(String companyName, String websiteUrl, String location){
+        this.companyName = companyName;
+        this.websiteUrl = websiteUrl;
+        this.location = location;
+    }
+
+}
