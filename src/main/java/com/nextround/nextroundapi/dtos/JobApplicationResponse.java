c2efbfada1ab304b50f23789ea46c38e37dda943
John Abuel <ninobano43@gmail.com>
2026-08-28 01:29:27 +0800

    feat: Created Job Application Response and started Job Application Service work-in-progress.

---
+++ src/main/java/com/nextround/nextroundapi/dtos/JobApplicationResponse.java
@@ -0,0 +1,26 @@
+package com.nextround.nextroundapi.dtos;
+
+import com.nextround.nextroundapi.entity.Company;
+import com.nextround.nextroundapi.entity.User;
+import com.nextround.nextroundapi.enums.ApplicationStatus;
+import com.nextround.nextroundapi.enums.WorkLocationType;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record JobApplicationResponse(UUID id, UserResponse user,
+                                     CompanyResponse company,
+                                     String jobTitle,
+                                     String jobURL,
+                                     ApplicationStatus applicationStatus,
+                                     WorkLocationType workLocation,
+                                     BigDecimal salaryMin,
+                                     BigDecimal salaryMax,
+                                     String currency,
+                                     LocalDate appliedDate,
+                                     LocalDateTime createdAt,
+                                     LocalDateTime updatedAt) {
+    }
+
---
+++ src/main/java/com/nextround/nextroundapi/service/JobApplicationService.java
@@ -0,0 +1,62 @@
+package com.nextround.nextroundapi.service;
+
+
+import com.nextround.nextroundapi.dtos.CompanyResponse;
+import com.nextround.nextroundapi.dtos.JobApplicationResponse;
+import com.nextround.nextroundapi.dtos.UserResponse;
+import com.nextround.nextroundapi.entity.JobApplication;
+import com.nextround.nextroundapi.repository.JobApplicationRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class JobApplicationService {
+    private JobApplicationRepository jobApplicationRepository;
+
+    JobApplicationService(JobApplicationRepository jobApplicationRepository){
+        this.jobApplicationRepository = jobApplicationRepository;
+    }
+
+    private JobApplicationResponse mapToDto(JobApplication jobApplication){
+        UserResponse userResponse = new UserResponse(
+                jobApplication.getUser().getId(),
+                jobApplication.getUser().getEmail(),
+                jobApplication.getUser().getFirstName(),
+                jobApplication.getUser().getLastName(),
+                jobApplication.getUser().getCreatedAt(),
+                jobApplication.getUser().getUpdatedAt()
+        );
+
+        CompanyResponse companyResponse = new CompanyResponse(
+                jobApplication.getCompany().getId(),
+                jobApplication.getCompany().getCompanyName(),
+                jobApplication.getCompany().getWebsiteUrl(),
+                jobApplication.getCompany().getIndustry(),
+                jobApplication.getCompany().getLocation(),
+                jobApplication.getCompany().getCreatedAt(),
+                jobApplication.getCompany().getUpdatedAt()
+        );
+
+
+
+        return new JobApplicationResponse(jobApplication.getId(),
+                userResponse,
+                companyResponse,
+                jobApplication.getJobTitle(),
+                jobApplication.getJobURL(),
+                jobApplication.getStatus(),
+                jobApplication.getWorkLocation(),
+                jobApplication.getSalaryMin(),
+                jobApplication.getSalaryMax(),
+                jobApplication.getCurrency(),
+                jobApplication.getAppliedDate(),
+                jobApplication.getCreatedAt(),
+                jobApplication.getUpdatedAt());
+    }
+
+    private JobApplication getJobApplicationIdInternal(UUID id){
+        return jobApplicationRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Job application with id: " + id + " does not exist."));
+    }
+
+}
