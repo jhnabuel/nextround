bc4033be2cb828a1413b7ac8a15ec53608b64ad2
John Abuel <ninobano43@gmail.com>
2026-09-01 01:38:18 +0800

    fix: added NoArgsConstructor annotation for User and fixed JobApplication request attribute typing.

--- src/main/java/com/nextround/nextroundapi/entity/User.java
+++ src/main/java/com/nextround/nextroundapi/entity/User.java
@@ -2,6 +2,7 @@ package com.nextround.nextroundapi.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -12,6 +13,7 @@ import java.util.UUID;
 @Setter
 @Getter
 @Entity
+@NoArgsConstructor
 @Table(name = "users")
 public class User {
 
