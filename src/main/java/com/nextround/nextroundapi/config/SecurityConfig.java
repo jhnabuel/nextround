741fd12f335174fc9d9eb9846f67cfdbbd3263d5
John Abuel <ninobano43@gmail.com>
2026-09-17 03:36:54 +0800

    feat: add explicit SecurityConfig with permissive filter chain.

--- src/main/java/com/nextround/nextroundapi/config/SecurityConfig.java
+++ src/main/java/com/nextround/nextroundapi/config/SecurityConfig.java
@@ -3,8 +3,12 @@ package com.nextround.nextroundapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
@@ -12,5 +16,14 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.
+                authorizeHttpRequests(auth -> auth.anyRequest().
+                        permitAll()).csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
     }
 }
