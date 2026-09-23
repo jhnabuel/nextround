718698183945586eb6337dd8fa173c639b0a1517
John Abuel <ninobano43@gmail.com>
2026-09-23 21:05:00 +0800

    feat: Implemented lookup function UserDetailService to fetch specific user from DB and AuthenticationManager.

--- src/main/java/com/nextround/nextroundapi/config/SecurityConfig.java
+++ src/main/java/com/nextround/nextroundapi/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.nextround.nextroundapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -25,5 +27,10 @@ public class SecurityConfig {
                         permitAll()).csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
     }
 }
